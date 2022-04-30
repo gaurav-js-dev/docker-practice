edd2f97be076acaf3feb0ec4b1cf35aa00be1960
Gaurav <gsharma.jai@salucro.com>
2022-04-30 21:30:42 +0530

    Added config and persistence mysql data storage volume

---
+++ blogger/config.php
@@ -0,0 +1,13 @@
+
+<?php
+
+define('DB_HOST', 'dbms');
+define('DB_NAME', 'MYSQL_DATABASE');
+define('DB_USER', 'MYSQL_USER');
+define('DB_PASS', 'MYSQL_PASSWORD');
+
+// To make contact form work please fill below details 
+
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USER', 'YOUR EMAIL');
+define('SMTP_PASS', 'YOUR EMAIL PASSWORD');
