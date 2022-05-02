f21d215f10b11f7ed4a01a63bff28b0a18257c80
Gaurav <gsharma.jai@salucro.com>
2022-05-03 00:17:06 +0530

    Updated seed data

--- blogger/dump/initdb.sql
+++ blogger/dump/initdb.sql
@@ -6,10 +6,21 @@ CREATE TABLE IF NOT EXISTS `article` (
   `content` text NOT NULL,
   `published_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;  
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ; 
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
 
 INSERT INTO `article`(`id`,`title`, `content`,`published_date`) VALUES
 (null,'How to use Docker','In this article, you will learn how to do MySql seeding using Docker Compose.','2021-08-31'),
 (null,'React 18','React 18, and what it means for the future. Our latest major version includes out-of-the-box improvements like automatic batching, new APIs like startTransition, and streaming server-side rendering with support for Suspense.','2022-08-31'),
 (null,'Is React Good','To give you a gentle introduction, React is an open-source JavaScript library used for frontend development, which was developed by Facebook.','2021-08-31');
 (null,'Is PHP Good','For those cases where trailing pad characters are stripped or comparisons ignore them, if a column has an index that requires unique values, inserting into the column values that differ only in number of trailing pad characters results in a duplicate-key error.','2021-08-31');
+
+INSERT INTO `user`(`id`,`username`, `password`) VALUES
+(null,'dave','$2a$04$qQyzO8o8G0z3EcezEjeaIennno0Xl4A2yw5fqerWloy0V8eipVHjm');
