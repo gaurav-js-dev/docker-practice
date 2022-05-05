d70be9d7dd908aeddf42d7ec1df08e76a37523a1
Gaurav <gsharma.jai@salucro.com>
2022-05-05 23:50:43 +0530

    Updated Dockerfile

--- laravel-cms/app.dockerfile
+++ laravel-cms/app.dockerfile
@@ -6,3 +6,6 @@ RUN apt-get update && apt-get install -y  \
     && pecl install imagick \
     && docker-php-ext-enable imagick \
     && docker-php-ext-install pdo_mysql
+
+ADD . /var/www
+RUN chown -R www-data:www-data /var/www
--- laravel-cms/resources/views/showpost.blade.php
+++ laravel-cms/resources/views/showpost.blade.php
@@ -5,11 +5,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Minimal Blog</title>
+    <title>Laravel CMS Blog</title>
     <meta name="author" content="name" />
     <meta name="description" content="description here" />
     <meta name="keywords" content="keywords,here" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet" />
 </head>
 
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
